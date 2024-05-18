9a19794e89d4198200bd3b5875a7307a4452101c
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2024-05-18 22:48:04 +0300

    Fixed landing. For the most part

--- src/app/seller/page.tsx
+++ src/app/seller/page.tsx
@@ -1,29 +1,20 @@
 import styles from "./landing.module.scss";
-import Image from "next/image";
 import Link from "next/link";
-import landingBackground from "/public/seller_landing_background.jpg";
-
-import Button from "@/components/GenericButton";
 
 export default function SellerLanding() {
   return (
     <>
-      <div className={styles.greetings}>
-        <div className={styles.text}>
-          <h1 className={styles.heading}>Помогайте дарить и радовать</h1>
-          <p className={styles.subheading}>Зарегистрируйтесь как продавец и продвигайте свои услуги</p>
+      <div className={styles.landingContainer}>
+        <div className={styles.foreground}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.heading}>Помогайте дарить и радовать</h1>
+            <p className={styles.subheading}>Зарегистрируйтесь как продавец и продвигайте свои услуги</p>
+          </div>
+          <Link href="/seller/login" className={styles.fuckingButton}>
+            <button className={styles.landingButton}>Стать продавцом!</button>
+          </Link>
         </div>
-        <Link href="/seller/login">
-          <Button text={ "Стать продавцом!" } />
-        </Link>
       </div>
-      <Image
-        className={styles.background}
-        src={landingBackground}
-        alt="Seller Landing Background"
-        priority
-        quality={100}
-      />
     </>
   );
 }
