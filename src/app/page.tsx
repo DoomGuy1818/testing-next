edced70ce4ebbbd0d36324c1943ac03beba64a65
Anatoly Pozdnyshev <pozdnyshev.anatol@gmail.com>
2024-05-19 19:39:11 +0300

    boockedGift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Header from "../components/Header/Header";
 import Main from "../components/Main/index"
 import Cube from "../components/fioletcube/index"
 import styles from "./page.module.css";
+
+import CreatingWish from "../components/BoockedGift/index"
 import HeaderGift from "../components/HeaderGift/HeaderGift"
-import CreatingWish from "../components/creatingWish/index"
 
 
 export default function Home() {
