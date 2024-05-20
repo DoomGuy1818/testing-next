2a91c7ba9475cae9ad0a6c296139f8ef14077875
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2024-05-20 12:19:49 +0300

    Fixing everything now! Hang tight!

--- src/app/seller/dashboard/service/[service_id]/page.tsx
+++ src/app/seller/dashboard/service/[service_id]/page.tsx
@@ -1,41 +1,34 @@
 import styles from "./dashboard.module.scss"
+import Image from "next/image"
 import Sidebar from "@/components/SellerDashboard/Sidebar"
 import Header from "@/components/SellerDashboard/Header"
-import Summary from "@/components/SellerDashboard/Summary"
-import SellerCard from "@/components/SellerDashboard/SellerCard"
 
-export default function SellerDashboard() {
+export default function CreateService() {
   return (
     <>
       <Sidebar/>
       <div className={styles.dashboardContainer}>
         <div className={styles.workSpace}>
-          <div className={styles.itemContainer}>
-            <Header text={ "Услуги" }/>
-            <div className={styles.itemGrid}>
-              <div className={styles.grid}>
-                <a href="/seller/dashboard/service/create">
-                  <div className={styles.itemWrap}>
-                    <SellerCard title={ "Добавить Услугу!" } image={ "/add.svg" } />
-                  </div>
-                </a>
-                <a href="/seller/dashboard/service/1">
-                  <div className={styles.itemWrap}>
-                    <SellerCard title={ "Упаковка подарков" } image={ "/packaging.png" } />
-                  </div>
-                </a>
-                <div className={styles.itemWrap}>
-                  <SellerCard title={ "Доставка на дом" } image={ "/delivery.png" } />
+          <Header text={ "Создать услугу" }/>
+          <div className={styles.createForm}>
+            <div className={styles.textRow}>
+              <div className={styles.textInputs}>
+                <input className={styles.input} placeholder={ "Упаковка подарков" }></input>
+                <input className={styles.input} placeholder={ "Ростов-на-Дону, ул. Свободного ПО, д.93" }></input>
+                <input className={styles.input} placeholder={ "1990" }></input>
+              </div>
+              <div className={styles.deleteButton}>
+                <div className={styles.deleteIcon}>
+                  <Image src="/delete.svg" alt="" width={50} height={50} />
                 </div>
-                <div className={styles.itemWrap}>
-                  <SellerCard title={ "Особая доставка" } image={ "/surprise.png" } />
+                <div className={styles.section}>
+                  Удалить услугу
                 </div>
               </div>
+              <button className={styles.createButton}>Изменить услугу</button>
             </div>
-          </div>
-          <div className={styles.summary}>
-            <Header text={ "Сводка" } />
-            <Summary revenue={ 3510 } reviewCount={ 15 } />
+            <div className={styles.photoBox}>
+            </div>
           </div>
         </div>
       </div>
